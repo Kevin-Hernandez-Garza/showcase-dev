0de2f4525c2c2d2bd79f16da6698f2fdce6d2db6
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2022-01-27 21:19:43 -0600

    updated code with comments and added Webvitals file

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+// importing the homepage jumbotron
 import coverImage from '../../assets/jumbotron.jpeg';
 
 function Home() {
     return (
-        <section>
+        <section className='my-5'>
             <img src={coverImage} style={{ width: "100%" }} alt='jumbotron' />
         </section>
     );
