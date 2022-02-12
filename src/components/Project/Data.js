1daf475ee298f302bebadea82daa9a7d2fce9682
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2022-02-12 03:03:53 -0600

    adding all projects

---
+++ src/components/Project/Data.js
@@ -0,0 +1,42 @@
+// const projects = [
+//     {
+//     img: require('../../assets/images/group-mock.png'), 
+//     alt: 'git-a-recipe application', 
+//     title: 'Git-A-Recipe', 
+//     description: 'Looking for a great meal for tonight!?', 
+//     github: "https://github.com/Kevin-Hernandez-Garza/recipe-app", 
+//     deployed: "https://adamromano89.github.io/recipe-app/"
+//     },
+//     {
+//         img: require('../../assets/images/group-mock.png'), 
+//         alt: 'git-a-recipe application', 
+//         title: 'Git-A-Recipe', 
+//         description: 'Looking for a great meal for tonight!?', 
+//         github: "https://github.com/Kevin-Hernandez-Garza/recipe-app", 
+//         deployed: "https://adamromano89.github.io/recipe-app/"
+//     },
+//     {
+//         img: require('../../assets/images/group-mock.png'), 
+//         alt: 'git-a-recipe application', 
+//         title: 'Git-A-Recipe', 
+//         description: 'Looking for a great meal for tonight!?', 
+//         github: "https://github.com/Kevin-Hernandez-Garza/recipe-app", 
+//         deployed: "https://adamromano89.github.io/recipe-app/"
+//     },
+// ];
+
+
+// const Projects = {
+//     project: [
+//         {
+//             img: "../../assets/images/group-mock.png",
+//             alt: 'git-a-recipe application',
+//             title: 'Git-A-Recipe', 
+//             description: 'Looking for a great meal for tonight!?', 
+//             github: "https://github.com/Kevin-Hernandez-Garza/recipe-app", 
+//             deployed: "https://adamromano89.github.io/recipe-app/"
+//         }
+//     ]
+// }
+
+// export default Projects;
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -19,12 +19,45 @@ const Project = (props) => {
             deployed: "https://adamromano89.github.io/recipe-app/"
         },
         {
-            img: require('../../assets/images/group-mock.png'), 
-            alt: 'git-a-recipe application', 
-            title: 'Git-A-Recipe', 
-            description: 'Looking for a great meal for tonight!?', 
+            img: require('../../assets/images/group-2.png'), 
+            alt: 'fragrance e-commerce store', 
+            title: 'BM Fragrance', 
+            description: 'All perfumes and colognes in one place!', 
+            github: "https://github.com/Kevin-Hernandez-Garza/group-2", 
+            deployed: "https://byteme-ecommerce.herokuapp.com/"
+        },
+        {
+            img: require('../../assets/images/horiseon-mockup.png'), 
+            alt: 'business firm', 
+            title: 'Horiseon', 
+            description: 'Business mock web application', 
+            deployed: "https://kevin-hernandez-garza.github.io/Horiseon-Refactor-Marketing-Site/", 
+            github: "https://github.com/Kevin-Hernandez-Garza/Horiseon-Refactor-Marketing-Site"
+        },
+        {
+            // fix
+            img: require('../../assets/images/runBuddy-mockup.png'), 
+            alt: 'personal trainer', 
+            title: 'Run Buddy', 
+            description: 'Need a personal trainer!?', 
             github: "https://github.com/Kevin-Hernandez-Garza/recipe-app", 
             deployed: "https://adamromano89.github.io/recipe-app/"
+        },
+        {
+            img: require('../../assets/images/webpage-screenshot.png'), 
+            alt: 'first website', 
+            title: 'My Journey', 
+            description: 'My first ever web application', 
+            github: "https://github.com/Kevin-Hernandez-Garza/firstproj", 
+            deployed: "https://kevin-hernandez-garza.github.io/firstproj/"
+        },
+        {
+            img: require('../../assets/images/coming-soon.jpeg'), 
+            alt: 'under construction', 
+            title: 'Coming Soon', 
+            description: 'Looking for a great meal for tonight!?', 
+            github: "https://github.com/Kevin-Hernandez-Garza", 
+            deployed: "https://github.com/Kevin-Hernandez-Garza"
         },
     ]);
 
