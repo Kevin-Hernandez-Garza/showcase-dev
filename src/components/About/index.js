45517807c91445ed896234391b3d67786f573f5e
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2022-03-24 03:59:20 -0500

    updated about section and the link for the new resume

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -23,7 +23,7 @@ const About = () => {
                 <TextWrapper>
                     <TopLine>About</TopLine>
                     <Heading>Hi, I'm Kevin</Heading>
-                    <Subtitle>Full-stack Web Developer who recently earned a certificate in Web Development from The University of Texas at Austin, with newly developed skills in JavaScript, CSS, HTML, React.js, and responsive web design. I am ambitious in facing problems head-on and learning new technologies, with a focus on mobile-first design and development. Excited to leverage my skills to become a skilled Software Engineer.</Subtitle>
+                    <Subtitle>A motivated programmer and recent graduate who went through a rigorous six-month program at <i>The University of Texas at Austin</i>. Gained the skillset to become a successful developer by learning the building blocks of web development and the MERN stack. Determined to become a skilled Software Engineer by implementing best practices and collaborating with inspiring people.</Subtitle>
                 </TextWrapper>
                 </Column1>
                 <Column2>
